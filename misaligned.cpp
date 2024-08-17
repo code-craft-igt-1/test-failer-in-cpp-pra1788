2499a3b37b86f5509359ec0a3ff880ecec8d922a
Praveen Kumar Tiwari <PraveenKumar.Tiwari@philips.com>
2024-08-17 15:22:34 +0530

    to pass misaligned color map

--- misaligned.cpp
+++ misaligned.cpp
@@ -48,7 +48,8 @@ void testFormatColorMap() {
     std::cout << "Generated Output:\n" << formattedOutput << "\n";
     std::cout << "Expected Output:\n" << expectedOutput << "\n";
 
-    assert(formattedOutput == expectedOutput && "Formatted output does not match the expected output");
+    assert(formattedOutput == expectedOutput && 
+                        "Formatted output does not match the expected output");
 }
 
 
